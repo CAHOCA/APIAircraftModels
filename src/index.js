d6d2b7402f157bc8f35241c6f0b4f0b38242ecac
cahoca <c-y-h-c702@hotmail.com>
2025-03-26 22:58:47 +0100

    add static homepage

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '.public/views')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+});
+
 app.use('/api', require('./api/modelAircraft'));
 
 
