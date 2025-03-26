35c88f9065b42fc117fc956f9f11b5a31248ac2c
Carlos Hortua Calderon <43701590+CAHOCA@users.noreply.github.com>
2025-03-26 23:06:18 +0100

    Update index.js 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const morgan=require('morgan');
+const path = require('path');
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
